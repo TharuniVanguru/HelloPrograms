53ab6f37c05c2238f6ecda9672a324f9a90a7a3d
TharuniVanguru <tharunireddy04@gmail.com>
2026-01-30 16:14:28 +0530

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,6 @@
+<html>
+  <head>
+  <script language="javascript">
+  var n1=parseInt(window.prompt("Enter first number"));
+  var n2=parseInt(window.prompt("Enter second number"));
+  document.write("
