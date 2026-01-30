c0f89043e363c76eb06acd8743fd1d3d09cc079f
TharuniVanguru <tharunireddy04@gmail.com>
2026-01-30 15:56:40 +0530

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,13 @@
+class calculator
+  {
+    public static void main (String args[])
+    {
+      int n1=Integer.parseInt(args[0]);
+      int n2=Integer.parseInt(args[1]);
+      System.out.println("Addition:"n1+n2);
+      System.out.println("Subtraction:"n1-n2);
+      System.out.println("Multiplicationb:"n1*n2);
+      System.out.println("Division:"n1/n2);
+    }
+  }
+      
